22153066cee8e8e0bdeb5db7d10eb1058d69c521
Maaz Rafique <maaz@devcrew.io>
2023-08-23 18:38:00 +0500

    perf: adds const keyword before constructor
    This change adds const keyword before facebook sign in button's class constructor

--- lib/src/social/fb_sign_in_button.dart
+++ lib/src/social/fb_sign_in_button.dart
@@ -13,8 +13,6 @@ enum ButtonColorTheme {
 /// A custom button widget that displays the Facebook logo and allows users to sign in with Facebook.
 class FacebookSignButton extends StatelessWidget {
   // Default background and foreground colors for the button.
-  Color _backgroundColor = const Color(0XFF1877F2);
-  Color _foregroundColor = Colors.white;
 
   /// The title or label of the button.
   final String title;
@@ -46,7 +44,7 @@ class FacebookSignButton extends StatelessWidget {
   /// The [onPressed] parameter must not be null.
   final VoidCallback onPressed;
 
-  FacebookSignButton({
+  const FacebookSignButton({
     Key? key,
     this.title = "Continue with Facebook",
     this.inCaps = false,
@@ -63,8 +61,7 @@ class FacebookSignButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Set the background and foreground colors based on the selected color theme.
-    _setColorTheme();
-
+    final (backgroundColor, foregroundColor) = _setColorTheme();
     return SizedBox(
       width: width,
       height: height,
@@ -79,9 +76,9 @@ class FacebookSignButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(radius),
           ),
           // Apply side border if the color theme is white with black or white with blue.
-          side: (buttonColorTheme == ButtonColorTheme.whiteWithBlack || buttonColorTheme == ButtonColorTheme.whiteWithBlue) ? BorderSide(color: _foregroundColor, width: 1.0): null,
-          foregroundColor: _foregroundColor,
-          backgroundColor: _backgroundColor,
+          side: (buttonColorTheme == ButtonColorTheme.whiteWithBlack || buttonColorTheme == ButtonColorTheme.whiteWithBlue) ? BorderSide(color: foregroundColor, width: 1.0): null,
+          foregroundColor: foregroundColor,
+          backgroundColor: backgroundColor,
           padding: const EdgeInsets.symmetric(horizontal: 0),
         ),
         child: Padding(
@@ -98,7 +95,7 @@ class FacebookSignButton extends StatelessWidget {
                   ImagePath.fbLogo,
                   package: 'get_buttons_stack',
                 ),
-                color: _foregroundColor,
+                color: foregroundColor,
                 // Maintain a size of the icon relative to the button's height.
                 width: (60 / 100 * height).floorToDouble(),
                 height: (60 / 100 * height).floorToDouble(),
@@ -121,24 +118,16 @@ class FacebookSignButton extends StatelessWidget {
   }
 
   // Set background and foreground colors based on the color theme.
-  void _setColorTheme() {
+  (Color,Color) _setColorTheme() {
     switch(buttonColorTheme) {
       case ButtonColorTheme.blueWithWhite:
-        _backgroundColor = const Color(0XFF1877F2);
-        _foregroundColor = Colors.white;
-        break; // Don't forget to break after each case.
+        return (const Color(0XFF1877F2), Colors.white);
       case ButtonColorTheme.blackWithWhite:
-        _backgroundColor = Colors.black;
-        _foregroundColor = Colors.white;
-        break;
+        return (Colors.black, Colors.white);
       case ButtonColorTheme.whiteWithBlue:
-        _backgroundColor = Colors.white;
-        _foregroundColor = const Color(0XFF1877F2);
-        break;
+        return (Colors.white, const Color(0XFF1877F2));
       case ButtonColorTheme.whiteWithBlack:
-        _backgroundColor = Colors.white;
-        _foregroundColor = Colors.black;
-        break;
+        return (Colors.white, Colors.black);
     }
   }
 }

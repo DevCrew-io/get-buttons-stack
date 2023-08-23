2a5167e90f05bf237b7053a11268083350498c4f
Maaz Rafique <maaz@devcrew.io>
2023-08-23 18:05:55 +0500

    refactor: optimise google sign in conditional statements
    This change optimise conditional statements in google sign in button class

--- lib/src/social/google_sign_in_button.dart
+++ lib/src/social/google_sign_in_button.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get_buttons_stack/src/utils/constants.dart';
 
-/// A custom button widget that displays the Google logo and allows users to sign in with Google.
 class GoogleSignInButton extends StatelessWidget {
-  /// The title or label of the button.
   final String title;
-
-  /// Flag to indicate if the title should be displayed in all uppercase letters.
   final bool isCaps;
-
-  /// The corner radius of the button.
   final double radius;
-
-  /// The optional width of the button.
   final double? width;
-
-  /// Flag to indicate if the button should have a dark theme.
   final bool isDark;
-
-  /// The height of the button.
   final double height;
-
-  /// Flag to indicate if the content of the button should be wrapped.
   final bool isContentWrapped;
-
-  /// Callback function to be executed when the button is pressed.
-  ///
-  /// The [onPressed] parameter must not be null.
   final VoidCallback onPressed;
-
 
   const GoogleSignInButton({
     Key? key,
@@ -44,6 +25,9 @@ class GoogleSignInButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textColor = isDark ? Colors.white : const Color(0xFF000000).withOpacity(0.54);
+    final bgColor = isDark ? const Color(0xFF4285F4) : const Color(0xFFFFFFFF);
+
     return SizedBox(
       width: width,
       height: height,
@@ -53,26 +37,20 @@ class GoogleSignInButton extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(radius),
           ),
-          foregroundColor: isDark
-              ? Colors.white
-              : const Color(0XFF000000).withOpacity(0.54),
-          backgroundColor: isDark
-              ? const Color(0XFF4285F4)
-              : const Color(0XFFFFFFFF),
+          foregroundColor: textColor,
+          backgroundColor: bgColor,
           padding: const EdgeInsets.symmetric(horizontal: 0),
         ),
         child: Padding(
           padding: const EdgeInsets.only(left: 1.5, right: 8),
           child: Row(
-            mainAxisSize:
-            isContentWrapped ? MainAxisSize.min : MainAxisSize.max,
+            mainAxisSize: isContentWrapped ? MainAxisSize.min : MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // Google logo container
               Container(
-                height: (height - 3),
-                width: (height - 3),
+                height: height - 3,
+                width: height - 3,
                 color: Colors.white.withOpacity(isDark ? 1.0 : 0.0),
                 child: const Image(
                   image: AssetImage(
@@ -81,8 +59,7 @@ class GoogleSignInButton extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(width: 8,),
-              // Text label
+              const SizedBox(width: 8),
               Text(
                 isCaps ? title.toUpperCase() : title,
                 style: const TextStyle(
